9183b704e9ae06ca649911d9454744b4d0966f65
theshadow2030 <hazem030500@gmail.com>
2025-05-12 12:56:48 +0200

    added tqdm

--- experiments_Minecraft.py
+++ experiments_Minecraft.py
@@ -13,6 +13,8 @@ from declare_formulas import formulas, formulas_names, max_length_traces
 from plot import plot_results, plot_results_all_formulas, state_distance_one_formula, state_distance_all_formulas
 import os
 
+from tqdm import tqdm
+from contextlib import redirect_stdout
 
 #flags
 absl.flags.DEFINE_integer("NUM_OF_SYMBOLS", 5, "number of symbols used to initialize the model")
@@ -88,20 +90,22 @@ def main(argv):
 
     image_seq_dataset = (train_img_seq, train_acceptance_img, test_img_seq_clss, test_acceptance_img_clss, test_img_seq_aut, test_acceptance_img_aut, test_img_seq_hard, test_acceptance_img_hard)
 
-    for i in range(num_exp):
-            #set_seed
+    with tqdm(range(num_exp)) as pbar:
+        for i in pbar:
             set_seed(9+i)
+            #set_seed
+            
             print("###################### NEW TEST ###########################")
             print("formula = {},\texperiment = {}".format(formula_name, i))
-
-            #DeepDFA
-            test_method("logic_circuit", formula, formula_name, dfa, symbolic_dataset, image_seq_dataset, i, log_dir=FLAGS.LOG_DIR, automata_dir=FLAGS.AUTOMATA_DIR, models_dir=FLAGS.MODELS_DIR)
-            #lstm
-            test_method("lstm", formula, formula_name, dfa, symbolic_dataset, image_seq_dataset, i, log_dir=FLAGS.LOG_DIR, models_dir=FLAGS.MODELS_DIR)
-            #gru
-            test_method("gru", formula, formula_name, dfa, symbolic_dataset, image_seq_dataset, i, log_dir=FLAGS.LOG_DIR, models_dir=FLAGS.MODELS_DIR)
-            #transformers
-            test_method("transformer", formula, formula_name, dfa, symbolic_dataset, image_seq_dataset, i, log_dir=FLAGS.LOG_DIR, models_dir=FLAGS.MODELS_DIR)
+            with open(os.devnull, 'w') as fnull, redirect_stdout(fnull):
+                #DeepDFA
+                test_method("logic_circuit", formula, formula_name, dfa, symbolic_dataset, image_seq_dataset, i, log_dir=FLAGS.LOG_DIR, automata_dir=FLAGS.AUTOMATA_DIR, models_dir=FLAGS.MODELS_DIR)
+                #lstm
+                test_method("lstm", formula, formula_name, dfa, symbolic_dataset, image_seq_dataset, i, log_dir=FLAGS.LOG_DIR, models_dir=FLAGS.MODELS_DIR)
+                #gru
+                test_method("gru", formula, formula_name, dfa, symbolic_dataset, image_seq_dataset, i, log_dir=FLAGS.LOG_DIR, models_dir=FLAGS.MODELS_DIR)
+                #transformers
+                test_method("transformer", formula, formula_name, dfa, symbolic_dataset, image_seq_dataset, i, log_dir=FLAGS.LOG_DIR, models_dir=FLAGS.MODELS_DIR)
     plot_results(formula, formula_name, res_dir = FLAGS.LOG_DIR,num_exp=num_exp, plot_legend=True, plot_dir= FLAGS.PLOTS_DIR, aut_dir=FLAGS.AUTOMATA_DIR, state_count=6, symbol_count=4)
 
 if __name__ == '__main__':
